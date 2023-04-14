5aeab86015d1a46d6772159e4dcb00638dd91e18
Ana-Ma-Web <90105049+Ana-Ma-Web@users.noreply.github.com>
2023-04-14 03:27:10 +0300

    feat: add hanoi task

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,30 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
+function createDreamTeam(members) {
   throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  
+  // let resultStr = ''
+  // let resultArr = []
+  // console.dir(members)
+  
+  // // if (typeof members === 'array') {
+  //   members.forEach(e => {
+  //     let firstLetter = e[0].toUpperCase()
+  //     resultStr += firstLetter
+  //     if (typeof [0] === 'string' && firstLetter === e[0]) {
+  //       resultStr += '123'
+  //       // resultArr.push(e[0])
+  //       // resultStr += firstLetter
+  //     }
+  //   })
+  //   // resultArr.sort()
+  //   // resultArr.forEach(e => {
+  //   //   resultStr += e
+  //   // })
+  // // }
+  // return resultStr
 }
 
 module.exports = {
--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -14,9 +14,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }
  *
  */
-function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new NotImplementedError('Not implemented');
+function calculateHanoi(disksNumber, turnsSpeed) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let minMoves = 0
+  for ( i = 0; i < disksNumber; i++){
+    minMoves = (minMoves * 2) + 1
+  }
+  let seconds = Math.floor(minMoves / (turnsSpeed / 60 / 60))
+
+  return { turns: minMoves, seconds}
 }
 
 module.exports = {
