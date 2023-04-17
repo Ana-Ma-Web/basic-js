db43cb03aa7527dfb6062177bbf99122924ddef6
Ana-Ma-Web <90105049+Ana-Ma-Web@users.noreply.github.com>
2023-04-17 07:58:48 +0300

    feat: add transform-array task 8/8

--- src/transform-array.js
+++ src/transform-array.js
@@ -16,9 +16,7 @@ const { NotImplementedError } = require('../extensions/index.js');
 function transform(startArr) {
   // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
-  console.debug(startArr)
   let resultArr = []
-
   if (Array.isArray(startArr)) {
     let arr = []
     startArr.forEach(el => {
@@ -26,42 +24,40 @@ function transform(startArr) {
     });
     for (let i = 0; i < arr.length; i++) {
       const el = arr[i];
+      if (typeof el !== 'undefined') {
 
-      switch (el) {
-        case '--discard-next':
-          i++
-          // arr[i + 1] = undefined
-          break;
-        case '--discard-prev':
-          resultArr.pop()
-          break;
-        case '--double-next':
-          if (typeof arr[i + 1] !== 'undefined') {
-            resultArr.push(arr[i + 1])
-
-          }
-          break;
-        case '--double-prev':
-          if (typeof resultArr[resultArr.length - 1] !== 'undefined') {
-            // if (resultArr[resultArr.length - 1] === arr[i - 1]) {
-              // resultArr.push(resultArr[resultArr.length - 1])
-              resultArr.push(arr[i - 1])
-            // }
-          }
-          break;
-
-        default:
-          if (typeof arr[i] !== 'undefined') {
-            resultArr.push(arr[i])
+        switch (el) {
+          case '--discard-next':
+            arr[i + 1] = undefined
             break;
-          }
+          case '--discard-prev':
+            if (arr[i - 1] === resultArr[resultArr.length - 1]) {
+              resultArr.pop()
+            }
+            break;
+          case '--double-next':
+            if (typeof arr[i + 1] !== 'undefined') {
+              resultArr.push(arr[i + 1])
+            }
+            break;
+          case '--double-prev':
+            if (typeof arr[i - 1] !== 'undefined') {
+              resultArr.push(arr[i - 1])
+            }
+            break;
+
+          default:
+            if (typeof arr[i] !== 'undefined') {
+              resultArr.push(arr[i])
+              break;
+            }
+        }
       }
     }
   } else {
     // return "'arr' parameter must be an instance of the Array!"
     throw new Error("'arr' parameter must be an instance of the Array!")
   }
-  console.debug(resultArr, 'resultArr')
   return resultArr
 }
 
